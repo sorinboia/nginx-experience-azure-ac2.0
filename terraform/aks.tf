a97d48c9ff660c7e353389a71040ef13ee9668a4
sorinboia <sorinboia@gmail.com>
2022-06-19 11:38:10 +0300

    1

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -20,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.agent_count
-    vm_size         = "Standard_D4_v3"
+    vm_size         = "Standard_E4a_v4"
   }
 
   #service_principal {
